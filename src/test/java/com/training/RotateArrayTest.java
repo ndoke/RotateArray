23ab021707741c582175027a7312e0f886d3e1a7
Nachiket Doke <ndoke@cisco.com>
2019-09-04 10:55:04 +0530

    Adds a few new test cases.

--- src/test/java/com/training/RotateArrayTest.java
+++ src/test/java/com/training/RotateArrayTest.java
@@ -8,11 +8,15 @@ public class RotateArrayTest {
     RotateArray rotateArray = new RotateArray();
     int input[];
     int output[];
+    int output1[];
+    int output2[];
 
     @BeforeMethod
     public void setUp() {
         input = new int[]{1, 2, 3, 4, 5};
         output = new int[]{4, 5, 1, 2, 3};
+        output1 = new int[]{2, 3, 4, 5, 1};
+        output2 = new int[]{5, 1, 2, 3, 4};
     }
 
     @Test
@@ -23,5 +27,25 @@ public class RotateArrayTest {
     @Test
     public void test2() {
         Assert.assertEquals(rotateArray.rotateBrute(input, 2), output);
+    }
+
+    @Test
+    public void test3() {
+        Assert.assertEquals(rotateArray.rotate(input, 4), output1);
+    }
+
+    @Test
+    public void test4() {
+        Assert.assertEquals(rotateArray.rotateBrute(input, 4), output1);
+    }
+
+    @Test
+    public void test5() {
+        Assert.assertEquals(rotateArray.rotate(input, 6), output2);
+    }
+
+    @Test
+    public void test6() {
+        Assert.assertEquals(rotateArray.rotateBrute(input, 6), output2);
     }
 }
